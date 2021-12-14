a4752cb04397d55d7b6e2109e674eda1451c73f5
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2021-12-13 22:52:43 -0700

    completed some tasks

--- math_dojo.py
+++ math_dojo.py
@@ -2,12 +2,26 @@ class MathDojo:
     def __init__(self):
         self.result = 0
     def add(self, num, *nums):
-        # your code here
+        self.result += num
+        for n in nums:
+            self.result += n
+        return self
     def subtract(self, num, *nums):
-        # your code here
+        self.result -= num 
+        for n in nums:
+            self.result -= n
+        return self
 # create an instance:
 md = MathDojo()
-# to test:
+
 x = md.add(2).add(2,5,1).subtract(3,2).result
-print(x)	# should print 5
-# run each of the methods a few more times and check the result!
+print(x)
+
+x = md.add(2,3,5,4,2).add(1,3,1).subtract(2,2,3,2,4,5).result
+print(x)
+
+x = md.add(3,4,2,5,2).add(5,4,2,5,1).subtract(5,4,6,2,3).result
+print(x)
+
+x = md.add(4,3,5,62).add(24,35,21).subtract(23,25,32).result
+print(x)
